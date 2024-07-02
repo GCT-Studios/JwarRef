bb4251cb90222a6bea36012fac3123ed794d8549
MCMBen <fleafa@gmail.com>
2024-07-02 13:43:08 +0100

    Quartz sync: Jul 2, 2024, 1:43 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,10 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "JwarRef GitHub": "https://github.com/GCT-Studios/JwarRef",
+      "Bushido Discord": "https://discord.gg/UeYGhJ9v",
+      "Quartz GitHub": "https://github.com/jackyzha0/quartz",
+      "Quartz Discord": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
